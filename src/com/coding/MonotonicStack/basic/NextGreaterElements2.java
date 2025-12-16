25e9c0d33191a6ccb4079078eef432fd58601dc2
weile <weilei@useepay.com>
2025-12-16 16:38:39 +0800

    20251216

--- src/com/coding/MonotonicStack/basic/NextGreaterElements2.java
+++ src/com/coding/MonotonicStack/basic/NextGreaterElements2.java
@@ -4,20 +4,30 @@ import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
 
+/**
+ * 下一个更大元素II
+ * index:503
+ * <a href="https://leetcode.cn/problems/next-greater-element-ii/description/">...</a>
+ */
 public class NextGreaterElements2 {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
-        int[] leftBigger = new int[n];
-        int[] rightBigger = new int[n];
-        Arrays.fill(leftBigger, -1);
-        Arrays.fill(rightBigger, -1);
-        Deque<Integer> stackRight = new ArrayDeque<>();
-        Deque<Integer> stackLeft = new ArrayDeque<>();
-        for (int left = 0; left < n; left++) {
-            // 同时计算两个栈
-            int right = n - left - 1;
-
+        // 下一个更大元素单调栈
+        Deque<Integer> deque = new ArrayDeque<>();
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+        // 循环范围 2n，模拟数组延长
+        for (int i = 0; i < 2 * n; i++) {
+            // 找到模拟延长的数组元素在原数组的位置
+            int index = i % n;
+            while (!deque.isEmpty() && nums[deque.peek()] < nums[index]) {
+                ans[deque.pop()] = nums[index];
+            }
+            // 大于 n 的数据是模拟的，没必要 push
+            if (i < n) {
+                deque.push(index);
+            }
         }
-        return nums;
+        return ans;
     }
 }
---
+++ src/com/coding/MonotonicStack/basic/StockSpanner.java
@@ -0,0 +1,40 @@
+package com.coding.MonotonicStack.basic;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 股票价格跨度
+ * index:901
+ * <a href="https://leetcode.cn/problems/online-stock-span/">...</a>
+ */
+public class StockSpanner {
+
+    /**
+     * 要找小于等于当前元素的股票价格，就是需要找到上一个比自己严格大的元素，所以要记录价格和价格的日期
+     */
+    private Deque<int[]> deque;
+
+    /**
+     * 当前日期
+     */
+    private int currentDay;
+
+    /**
+     * 初始化类的变量，此方法测试的时候一个case只会调用一次，后面都是复用类，否则没法实现需求了
+     */
+    public StockSpanner() {
+        deque = new ArrayDeque<>();
+        currentDay = -1;
+    }
+
+    public int next(int price) {
+        currentDay++; // 当前日期自增
+        while (!deque.isEmpty() && deque.peek()[0] <= price) {
+            deque.pop();
+        }
+        int span = deque.isEmpty() ? currentDay + 1 : currentDay - deque.peek()[1];
+        deque.push(new int[] { price, currentDay}); // 当前元素入栈
+        return span;
+    }
+}
